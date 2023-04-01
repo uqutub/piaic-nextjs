7f6d01f6177379d991f41cc2f667b518cfdf4a0e
Yousuf Qutubuddin <yousuf.qutubuddin@jibe.solutions>
2023-04-01 22:28:19 +0500

    Basic Routing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 
@@ -15,6 +16,14 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
+        <ul>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
         <div className={styles.description}>
           <p>
             Get started by editing&nbsp;
